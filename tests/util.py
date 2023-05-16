4b637a3210ec6e2617604fbb7e5f8de535441301
MasloMaslane <m.masiarz@fri.edu.pl>
2023-05-16 19:49:20 +0000

    move simple package to subdirectory

--- tests/util.py
+++ tests/util.py
@@ -1,5 +1,5 @@
 import os
 
 def get_simple_package_path():
-    """Get path to simple package (/tests/abc)"""
-    return os.path.join(os.path.dirname(__file__), "abc")
+    """Get path to simple package (/tests/packages/abc)"""
+    return os.path.join(os.path.dirname(__file__), "packages", "abc")
