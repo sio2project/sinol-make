cf979fce6e34ae19f7dee1af5ee2939e81ccf73e
Mateusz Masiarz <matimasiarz@gmail.com>
2023-06-13 21:05:21 +0200

    Simplify help messages
    Co-authored-by: Tomasz Nowak <36604952+tonowak@users.noreply.github.com>

--- src/sinol_make/commands/run/__init__.py
+++ src/sinol_make/commands/run/__init__.py
@@ -41,9 +41,9 @@ class Command(BaseCommand):
 		parser.add_argument('--oiejq_path', type=str,
 		      				help='path to oiejq executable (default: `~/.local/bin/oiejq`)')
 		parser.add_argument('--c_compiler_path', type=str, default=compiler.get_c_compiler_path(),
-		    				help='C compiler to use (default for Linux and Windows: gcc, default for Mac: gcc-{9,10})')
+		    				help='C compiler to use (default for Linux and Windows: gcc, default for Mac: gcc-9 or gcc-10)')
 		parser.add_argument('--cpp_compiler_path', type=str, default=compiler.get_cpp_compiler_path(),
-		    				help='C++ compiler to use (default for Linux and Windows: g++, default for Mac: g++-{9,10})')
+		    				help='C++ compiler to use (default for Linux and Windows: g++, default for Mac: g++-9 or g++-10)')
 		parser.add_argument('--python_interpreter_path', type=str, default=compiler.get_python_interpreter_path(),
 		    				help='Python interpreter to use (default: python3)')
 		parser.add_argument('--java_compiler_path', type=str, default=compiler.get_java_compiler_path(),
