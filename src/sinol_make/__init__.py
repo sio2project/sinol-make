663ff997c0c67d479ae816a92a05da8c3ddab4e6
MasloMaslane <matimasiarz@gmail.com>
2023-06-14 12:52:30 +0200

    Install oiejq automatically

--- src/sinol_make/__init__.py
+++ src/sinol_make/__init__.py
@@ -1,4 +1,6 @@
 import argparse
+import sys
+
 from sinol_make import util
 
 __version__ = "1.0.0"
@@ -30,6 +32,20 @@ def main():
 
 	for command in commands:
 		if command.get_name() == args.command:
+			if sys.platform == 'linux' and not util.check_oiejq():
+				print(util.warning('`oiejq` is not installed. It will be installed now.'))
+
+				try:
+					if util.install_oiejq():
+						print(util.info('`oiejq` was successfully installed.'))
+					else:
+						print(util.error('`oiejq` could not be installed. You can try installing it manually.'))
+						exit(1)
+				except Exception as e:
+					print(util.error('`oiejq` could not be installed.'))
+					print(util.error(e))
+					exit(1)
+
 			command.run(args)
 			exit(0)
 
