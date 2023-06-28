a0a17c7778bf06d7ac793296d4dfcabed2079cb2
Mateusz Masiarz <matimasiarz@gmail.com>
2023-06-28 12:21:03 +0200

    Add second inwer for test package

---
+++ tests/packages/wer/prog/werinwer2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    if (argc != 2) {
+        cout << "ERROR: Invalid number of arguments" << endl;
+        return 1;
+    }
+
+    int n, a, s = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        s += a;
+
+        if (a >= n) {
+            cout << "ERROR: " << a << " >= " << n << "\n";
+            return 1;
+        }
+    }
+    if (n == 3 && s != 5) {
+        cout << "ERROR: " << s << " != 5\n";
+        return 1;
+    }
+
+    cout << "OK" << endl << "Group " << n - 1 << endl << "Test " + (string)argv[1] << endl;
+    return 0;
+}
