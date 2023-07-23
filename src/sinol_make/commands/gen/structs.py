45f1fd3c52e501889a1272f888266bf95835432c
Mateusz Masiarz <matimasiarz@gmail.com>
2023-07-23 16:23:26 +0200

    Add comments for struct

--- src/sinol_make/commands/gen/structs.py
+++ src/sinol_make/commands/gen/structs.py
@@ -6,6 +6,9 @@ class OutputGenerationArguments:
     """
     Arguments used for function that generates output file.
     """
+    # Path to correct solution executable
     correct_solution_exe: str
+    # Path to input file
     input_test: str
+    # Path to output file
     output_test: str
