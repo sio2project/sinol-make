be168c6a847dc5c57dd07773abbe73b3a6bc6b0a
Mateusz Masiarz <matimasiarz@gmail.com>
2023-07-23 17:01:02 +0200

    Bash parallel input generation

--- tests/packages/gen/prog/gen.cpp
+++ tests/packages/gen/prog/gen.cpp
@@ -2,8 +2,12 @@
 
 using namespace std;
 
-int main() {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b;
+int main(int argc, char const *argv[]) {
+    if (argc != 2) {
+        return 1;
+    }
+
+    int c = argv[1][4] - 'a';
+    int g = argv[1][3] - '0';
+    cout << c  << " " << g << endl;
 }
