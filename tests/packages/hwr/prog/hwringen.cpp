387e9bafca0a2a48ffe231df592cbbeed4eda8cb
Mateusz Masiarz <matimasiarz@gmail.com>
2023-08-06 17:45:56 +0200

    Add package for testing `export` command

---
+++ tests/packages/hwr/prog/hwringen.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ofstream f;
+
+    f.open("hwr1a.in");
+    f << "20 2\n";
+    f.close();
+
+    f.open("hwr2a.in");
+    f << "53 6\n";
+    f.close();
+}
