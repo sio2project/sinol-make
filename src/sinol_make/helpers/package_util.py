19ec11576ad7350806f74aed32edc4b773c66f04
Mateusz Masiarz <matimasiarz@gmail.com>
2023-09-02 21:31:33 +0200

    Change `package_util.get_task_id` function

--- src/sinol_make/helpers/package_util.py
+++ src/sinol_make/helpers/package_util.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 import glob
 from enum import Enum
 from typing import List, Union, Dict, Any
@@ -7,13 +8,17 @@ from sinol_make import util
 
 
 def get_task_id() -> str:
-    task_id = os.path.split(os.getcwd())[-1]
-    if len(task_id) == 3:
-        return task_id
+    with open(os.path.join(os.getcwd(), "config.yml")) as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    if "sinol_task_id" in config:
+        return config["sinol_task_id"]
     else:
-        model_solution = glob.glob(os.path.join(os.getcwd(), "prog", "???.*"))
-        if len(model_solution) == 1:
-            return os.path.basename(model_solution[0])[:3]
+        print(util.warning("sinol_task_id not specified in config.yml. Using task id from directory name."))
+        task_id = os.path.split(os.getcwd())[-1]
+        if len(task_id) == 3:
+            return task_id
+        else:
+            util.exit_with_error("Invalid task id. Task id should be 3 characters long.")
 
 
 def extract_test_id(test_path):
