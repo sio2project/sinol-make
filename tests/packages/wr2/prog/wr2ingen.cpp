6d167dd51c93ae13d3ffb1856f44a10547640fc5
Mateusz Masiarz <matimasiarz@gmail.com>
2023-09-02 11:03:22 +0200

    Add package with inwer with no output

---
+++ tests/packages/wr2/prog/wr2ingen.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ofstream f;
+    f.open("wr21a.in");
+    f << "2\n0 1\n";
+    f.close();
+
+    f.open("wr22a.in");
+    f << "3\n2 1 1\n";
+    f.close();
+
+    f.open("wr23a.in");
+    f << "4\n1 1 3 2\n";
+    f.close();
+}
