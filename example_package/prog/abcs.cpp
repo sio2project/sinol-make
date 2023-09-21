ec5cd4132c40877ec475ec1769904e9e66288423
Mateusz Masiarz <matimasiarz@gmail.com>
2023-09-21 17:30:07 +0200

    Add an example package

---
+++ example_package/prog/abcs.cpp
@@ -0,0 +1,11 @@
+// This is a "brute force" solution for testing model solution.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+}
