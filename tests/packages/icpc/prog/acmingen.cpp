0bde0d214a9efbcbba2b7235266d6cf80690402f
Mateusz Masiarz <matimasiarz@gmail.com>
2023-09-23 14:48:53 +0200

    Add icpc package

---
+++ tests/packages/icpc/prog/acmingen.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ofstream f("acm1a.in");
+	f << "1 3\n";
+	f.close();
+	f.open("acm2a.in");
+	f << "2 5\n";
+	f.close();
+	f.open("acm3a.in");
+	f << "3 7\n";
+	f.close();
+	f.open("acm4a.in");
+	f << "4 9\n";
+	f.close();
+}
