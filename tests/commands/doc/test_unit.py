4043bacc679f232385e77af3b7009b795243ee70
Mateusz Masiarz <matimasiarz@gmail.com>
2023-12-02 22:29:03 +0100

    Fix doc tests

--- tests/commands/doc/test_unit.py
+++ tests/commands/doc/test_unit.py
@@ -1,12 +1,11 @@
 import os
 import pytest
 
-from sinol_make.commands.doc import Command
 from tests.fixtures import create_package
 from tests import util
 
 
 @pytest.mark.parametrize("create_package", [util.get_doc_package_path()], indirect=True)
 def test_compile_file(create_package):
-    command = Command()
+    command = util.get_doc_command()
     assert command.compile_file(os.path.abspath(os.path.join(os.getcwd(), "doc/doczad.tex"))) is True
