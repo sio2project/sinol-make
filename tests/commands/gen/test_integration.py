ed82922556020025bde21cbb891c568d430b5738
Mateusz Masiarz <matimasiarz@gmail.com>
2023-12-02 22:29:21 +0100

    Fix gen tests

--- tests/commands/gen/test_integration.py
+++ tests/commands/gen/test_integration.py
@@ -1,24 +1,27 @@
 import sys
-
 import yaml
 import glob
 
-from sinol_make import configure_parsers
 from sinol_make import util as sm_util
 from sinol_make.commands.gen import Command
-from sinol_make.commands.gen import gen_util
 from sinol_make.helpers import package_util, paths, cache
+from sinol_make.programs.ingen import Ingen
+from sinol_make.programs.solution import Solution
 from tests.fixtures import *
 from tests import util
 
 
-def simple_run(arguments=None):
+def run(arguments=None):
     if arguments is None:
         arguments = []
-    parser = configure_parsers()
-    args = parser.parse_args(["gen"] + arguments)
-    command = Command()
-    command.run(args)
+    arguments = ["gen"] + arguments
+    util.run(Command, arguments)
+
+
+def get_command() -> Command:
+    command = util.get_gen_command()
+    command.base_run(None)
+    return command
 
 
 def get_md5_sums(package_path):
@@ -35,10 +38,10 @@ def test_simple(capsys, create_package):
     """
     Test `ingen` command with no parameters on package with no tests.
     """
-    simple_run()
-
+    run()
     out = capsys.readouterr().out
     assert "Successfully generated input files." in out
+    sys.stdout.write(out)
     assert "Successfully generated all output files." in out
 
 
@@ -49,13 +52,13 @@ def test_correct_inputs(capsys, create_package):
     Test `ingen` command with all unchanged inputs.
     """
     task_id = package_util.get_task_id()
-    correct_solution = package_util.get_correct_solution(task_id)
+    correct_solution = Solution.get_correct_solution_file(task_id)
     cache.save_compiled(correct_solution, "exe")
-    simple_run()
+    run()
     md5_sums = get_md5_sums(create_package)
 
-    # Run again to check if all inputs are unchanged.
-    simple_run()
+    # Run again to check if all outputs are unchanged.
+    run()
     out = capsys.readouterr().out
     assert "All output files are up to date." in out
     assert md5_sums == get_md5_sums(create_package)
@@ -68,9 +71,9 @@ def test_changed_inputs(capsys, create_package):
     Test `ingen` command with changed inputs.
     """
     task_id = package_util.get_task_id()
-    correct_solution = package_util.get_correct_solution(task_id)
+    correct_solution = Solution.get_correct_solution_file(task_id)
     cache.save_compiled(correct_solution, "exe")
-    simple_run()
+    run()
     md5_sums = get_md5_sums(create_package)
     correct_md5 = md5_sums.copy()
 
@@ -83,7 +86,7 @@ def test_changed_inputs(capsys, create_package):
         yaml.dump(md5_sums, f)
     sys.stdout.write(str(md5_sums))
 
-    simple_run()
+    run()
     out = capsys.readouterr().out
     assert "Generating output files for 2 tests" in out
     for file in ins[:2]:
@@ -97,13 +100,12 @@ def test_shell_ingen_unchanged(create_package):
     """
     Test if ingen.sh is unchanged after running `ingen` command.
     """
-    package_path = create_package
-    task_id = package_util.get_task_id()
-    shell_ingen_path = gen_util.get_ingen(task_id)
-    assert os.path.splitext(shell_ingen_path)[1] == ".sh"
-    edited_time = os.path.getmtime(shell_ingen_path)
-    simple_run()
-    assert edited_time == os.path.getmtime(shell_ingen_path)
+    command = get_command()
+    shell_ingen: Ingen = command.get_ingen()
+    assert shell_ingen.is_shell_ingen()
+    edited_time = os.path.getmtime(shell_ingen.file_path)
+    run()
+    assert edited_time == os.path.getmtime(shell_ingen.file_path)
 
 
 @pytest.mark.parametrize("create_package", [util.get_shell_ingen_pack_path(), util.get_simple_package_path()],
@@ -112,7 +114,7 @@ def test_only_inputs_flag(create_package):
     """
     Test if `--only-inputs` flag works.
     """
-    simple_run(["--only-inputs"])
+    run(["--only-inputs"])
     ins = glob.glob(os.path.join(create_package, "in", "*.in"))
     outs = glob.glob(os.path.join(create_package, "out", "*.out"))
     assert len(ins) > 0
@@ -125,7 +127,7 @@ def test_only_outputs_flag(create_package):
     """
     Test if `--only-outputs` flag works.
     """
-    simple_run(['--only-inputs'])
+    run(['--only-inputs'])
     ins = glob.glob(os.path.join(create_package, "in", "*.in"))
     outs = glob.glob(os.path.join(create_package, "out", "*.out"))
     in1 = ins[0]
@@ -135,7 +137,7 @@ def test_only_outputs_flag(create_package):
     def in_to_out(file):
         return os.path.join(create_package, "out", os.path.basename(file).replace(".in", ".out"))
 
-    simple_run(["--only-outputs"])
+    run(["--only-outputs"])
     ins = glob.glob(os.path.join(create_package, "in", "*.in"))
     outs = glob.glob(os.path.join(create_package, "out", "*.out"))
     assert len(ins) == 1
@@ -151,11 +153,11 @@ def test_missing_output_files(create_package):
     """
     package_path = create_package
     for args in [[], ["--only-outputs"]]:
-        simple_run()
+        run()
         outs = glob.glob(os.path.join(package_path, "out", "*.out"))
         os.unlink(outs[0])
         assert not os.path.exists(outs[0])
-        simple_run(args)
+        run(args)
         assert os.path.exists(outs[0])
         shutil.rmtree(paths.get_cache_path())
         os.unlink(os.path.join(package_path, "in", ".md5sums"))
@@ -170,18 +172,19 @@ def test_correct_solution_changed(create_package):
     package_path = create_package
     task_id = package_util.get_task_id()
     md5sums = os.path.join(package_path, "in", ".md5sums")
-    simple_run()
+    run()
     assert os.path.exists(md5sums)
     outputs = {}
     for output in glob.glob(os.path.join(package_path, "out", "*.out")):
         outputs[os.path.basename(output)] = sm_util.get_file_md5(output)
 
-    solution = package_util.get_correct_solution(task_id)
-    with open(os.path.join(solution), "w") as f:
+    command = get_command()
+    solution: Solution = command.get_solution()
+    with open(os.path.join(solution.file_path), "w") as f:
         f.write("int main() {}")
     cache.check_correct_solution(task_id)
     assert not os.path.exists(md5sums)
-    simple_run()
+    run()
     assert os.path.exists(md5sums)
     for output in glob.glob(os.path.join(package_path, "out", "*.out")):
         assert outputs[os.path.basename(output)] != sm_util.get_file_md5(output)
@@ -194,8 +197,8 @@ def test_fsanitize(create_package):
     """
     if sm_util.is_macos_arm():
         pytest.skip("-fsanitize=address,undefined is not supported on Apple Silicon")
-    for ingen in ["prog/geningen3.cpp", "prog/geningen4.cpp"]:
+    for ingen in ["prog/geningen2.cpp", "prog/geningen3.cpp"]:
         with pytest.raises(SystemExit) as e:
-            simple_run([ingen])
+            run([ingen])
         assert e.type == SystemExit
         assert e.value.code == 1
