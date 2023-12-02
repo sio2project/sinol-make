81c1b21a32be71394864143ef38b1d0ec83249e8
Mateusz Masiarz <matimasiarz@gmail.com>
2023-12-02 22:29:27 +0100

    Fix inwer tests

--- tests/commands/inwer/test_unit.py
+++ tests/commands/inwer/test_unit.py
@@ -1,33 +1,11 @@
-import argparse
 import re
 
-from ...fixtures import *
-from ... import util
-from sinol_make.helpers import package_util, compiler
+from sinol_make.tests.input import InputTest
+from tests.fixtures import *
+from tests import util
+from sinol_make.helpers import package_util
 from sinol_make.commands.inwer import inwer_util, InwerExecution
 from sinol_make.commands.inwer import Command
-
-
-def test_get_inwer_path():
-    """
-    Test getting default and custom inwer.
-    """
-    os.chdir(util.get_inwer_package_path())
-    task_id = package_util.get_task_id()
-    assert inwer_util.get_inwer_path(task_id) is not None
-    assert inwer_util.get_inwer_path(task_id, 'prog/werinwer2.cpp') == os.path.join(os.getcwd(), 'prog', 'werinwer2.cpp')
-
-
-@pytest.mark.parametrize("create_package", [util.get_inwer_package_path()], indirect=True)
-def test_compile_inwer(create_package):
-    """
-    Test compilation of inwer.
-    """
-    task_id = package_util.get_task_id()
-    inwer_path = inwer_util.get_inwer_path(task_id)
-    args = compiler.get_default_compilers()
-    executable, compile_log = inwer_util.compile_inwer(inwer_path, args)
-    assert os.path.exists(executable)
 
 
 @pytest.mark.parametrize("create_package", [util.get_inwer_package_path()], indirect=True)
@@ -38,26 +16,20 @@ def test_asserting_inwer(create_package):
     package_path = create_package
     task_id = package_util.get_task_id()
     util.create_ins(package_path, task_id)
-    inwer_path = os.path.join(os.getcwd(), 'prog', 'werinwer3.cpp')
-    args = argparse.Namespace(
-        c_compiler_path=compiler.get_c_compiler_path(),
-        cpp_compiler_path=compiler.get_cpp_compiler_path(),
-        python_interpreter_path=compiler.get_python_interpreter_path(),
-        java_compiler_path=compiler.get_java_compiler_path()
-    )
-    executable, compile_log = inwer_util.compile_inwer(inwer_path, args)
-
+    command = util.get_inwer_command()
+    command.base_run(None)
+    inwer = command.get_inwer("prog/werinwer3.cpp")
+    exe, _ = inwer.compile()
+    assert exe is not None
+    test = InputTest(task_id, "in/wer2a.in")
     execution = InwerExecution(
-        inwer_exe_path=executable,
-        test_name='wer2a.in',
-        test_path=os.path.join(os.getcwd(), 'in', 'wer2a.in'),
+        inwer=inwer,
+        test=test,
     )
 
     res = Command.verify_test(execution)
     assert res.valid is False
     assertion_re = re.compile(r'.*Assertion.*failed')
-    print(res.output)
-    print(assertion_re.match(res.output))
     assert assertion_re.match(res.output) is not None
 
 
@@ -67,39 +39,42 @@ def test_tests_comparator():
         assert inwer_util.sort_tests([f"{ti}2a.in", f"{ti}1a.in", f"{ti}1b.in"], ti) == \
                [f"{ti}1a.in", f"{ti}1b.in", f"{ti}2a.in"]
         assert inwer_util.sort_tests([f"{ti}2a.in", f"{ti}1a.in", f"{ti}1b.in", f"{ti}10a.in"], ti) == \
-                [f"{ti}1a.in", f"{ti}1b.in", f"{ti}2a.in", f"{ti}10a.in"]
+               [f"{ti}1a.in", f"{ti}1b.in", f"{ti}2a.in", f"{ti}10a.in"]
         assert inwer_util.sort_tests([f"{ti}2a.in", f"{ti}1a.in", f"{ti}1b.in", f"{ti}10a.in", f"{ti}10b.in"], ti) == \
-                [f"{ti}1a.in", f"{ti}1b.in", f"{ti}2a.in", f"{ti}10a.in", f"{ti}10b.in"]
+               [f"{ti}1a.in", f"{ti}1b.in", f"{ti}2a.in", f"{ti}10a.in", f"{ti}10b.in"]
 
 
 def test_verify_tests_order():
-    command = Command()
+    def convert_arr_to_tests(tests):
+        return [InputTest("abc", test, exists=False) for test in tests]
+
+    os.chdir(util.get_simple_package_path())
+    command = util.get_inwer_command()
     command.task_id = "abc"
-    command.tests = ["abc1ocen.in", "abc2ocen.in", "abc3ocen.in",
-                     "abc1a.in", "abc1b.in", "abc1c.in", "abc1d.in",
-                     "abc2z.in", "abc2aa.in", "abc2ab.in", "abc2ac.in"]
+    command.tests = convert_arr_to_tests(["abc1ocen.in", "abc2ocen.in", "abc3ocen.in",
+                                          "abc1a.in", "abc1b.in", "abc1c.in", "abc1d.in",
+                                          "abc2z.in", "abc2aa.in", "abc2ab.in", "abc2ac.in"])
     command.verify_tests_order()
 
-    command.tests.remove("abc2ocen.in")
+    command.tests = convert_arr_to_tests(["abc1ocen.in", "abc3ocen.in",
+                                          "abc1a.in", "abc1b.in", "abc1c.in", "abc1d.in",
+                                          "abc2z.in", "abc2aa.in", "abc2ab.in", "abc2ac.in"])
     with pytest.raises(SystemExit):
         command.verify_tests_order()
 
-    command.tests.append("abc2ocen.in")
-    command.tests.remove("abc1c.in")
+    command.tests = convert_arr_to_tests(["abc1ocen.in", "abc2ocen.in", "abc3ocen.in",
+                                          "abc1a.in", "abc1b.in", "abc1d.in",
+                                          "abc2z.in", "abc2aa.in", "abc2ab.in", "abc2ac.in"])
     with pytest.raises(SystemExit):
         command.verify_tests_order()
 
-    command.tests.append("abc1c.in")
-    command.tests.remove("abc2aa.in")
+    command.tests = convert_arr_to_tests(["abc1ocen.in", "abc2ocen.in", "abc3ocen.in",
+                                          "abc1a.in", "abc1b.in", "abc1c.in", "abc1d.in",
+                                          "abc2z.in", "abc2ab.in", "abc2ac.in"])
     with pytest.raises(SystemExit):
         command.verify_tests_order()
 
-    command.tests.append("abc2aa.in")
-    command.tests.remove("abc1ocen.in")
-    command.tests.remove("abc2ocen.in")
-    command.tests.remove("abc3ocen.in")
-    command.tests.append("abc9ocen.in")
-    command.tests.append("abc10ocen.in")
-    command.tests.append("abc11ocen.in")
-
+    command.tests = convert_arr_to_tests(["abc9ocen.in", "abc10ocen.in", "abc11ocen.in",
+                                          "abc1a.in", "abc1b.in", "abc1c.in", "abc1d.in",
+                                          "abc2z.in", "abc2aa.in", "abc2ab.in", "abc2ac.in"])
     command.verify_tests_order()
