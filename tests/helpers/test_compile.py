ac56eb1313f98a3e88c3fe6f1254b36bf7006c5f
Mateusz Masiarz <matimasiarz@gmail.com>
2024-01-02 12:21:43 +0100

    Add tests

--- tests/helpers/test_compile.py
+++ tests/helpers/test_compile.py
@@ -1,7 +1,11 @@
 import os
+import pytest
 import tempfile
 
 from sinol_make.helpers.cache import save_compiled, check_compiled
+from tests import util
+from tests.fixtures import create_package
+from tests.commands.gen.test_integration import simple_run
 
 
 def test_compilation_caching():
@@ -16,3 +20,18 @@ def test_compilation_caching():
         save_compiled(os.path.join(os.getcwd(), "test.txt"),
                       os.path.join(os.getcwd(), "test.e"))
         assert check_compiled(os.path.join(os.getcwd(), "test.txt")) == os.path.join(os.getcwd(), "test.e")
+
+
+@pytest.mark.parametrize("create_package", [util.get_shell_ingen_pack_path()], indirect=True)
+def test_large_failed_output(create_package):
+    """
+    Test `ingen` command with large failed output.
+    """
+    with open(os.path.join(create_package, "prog", "geningen2.cpp"), "r") as f:
+        code = f.read()
+    code.replace("<<", "<");
+    with open(os.path.join(create_package, "prog", "geningen2.cpp"), "w") as f:
+        f.write(code)
+
+    with pytest.raises(SystemExit):
+        simple_run(["prog/geningen2.cpp"])
