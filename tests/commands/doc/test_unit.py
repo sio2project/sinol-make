822a9c67e1563ea9a2006fceb0710ccddad18dcb
Stanisław Czech <sc@lo3gdynia.pl>
2024-02-20 21:36:12 +0100

    Pdflatex compilation

--- tests/commands/doc/test_unit.py
+++ tests/commands/doc/test_unit.py
@@ -7,6 +7,11 @@ from tests import util
 
 
 @pytest.mark.parametrize("create_package", [util.get_doc_package_path()], indirect=True)
-def test_compile_file(create_package):
+def test_compile_pdf_latex(create_package):
     command = Command()
-    assert command.compile_file(os.path.abspath(os.path.join(os.getcwd(), "doc/doczad.tex"))) is True
+    assert command.compile_pdf_latex(os.path.abspath(os.path.join(os.getcwd(), "doc/doczad.tex"))) is True
+
+@pytest.mark.parametrize("create_package", [util.get_doc_package_path()], indirect=True)
+def test_compile_file_latex_div(create_package):
+    command = Command()
+    assert command.compile_file_latex_div(os.path.abspath(os.path.join(os.getcwd(), "doc/doczad.tex"))) is True
