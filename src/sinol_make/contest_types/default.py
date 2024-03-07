48c560b078bef9726bbf5f684dfea25ff0ddd546
Stanisław Czech <sc@lo3gdynia.pl>
2024-03-07 18:10:08 +0100

    Extend contest API (#202)
    * Extend contest API
    allow for contest specific features on export.
    
    * API for config verification

--- src/sinol_make/contest_types/default.py
+++ src/sinol_make/contest_types/default.py
@@ -100,3 +100,17 @@ class DefaultContest:
         :return: Global score
         """
         return sum(group["points"] for group in groups_scores.values())
+    
+    def verify_config(self):
+        """
+        Used for verifing contest specific config.yml settings
+        """
+        pass
+
+    def additional_export_job(self):
+        """
+        Called once during package export, just before compressing it to archive.
+        Allows for contest specific jobs before during package export.
+        :return: If not None, returned value will be used as name of the archive
+        """
+        return None
