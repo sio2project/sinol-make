ea6ec3905a5550dfcbf5a20cec642bc418044173
Mateusz Masiarz <matimasiarz@gmail.com>
2024-03-08 10:37:04 +0100

    Remove dangling input files (#209)
    * Remove dangling files
    
    * Add tests
    
    * Fix tests

---
+++ tests/packages/gen/prog/geningen5.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ofstream f("gen1.in");
+    f << "1 2\n";
+    f.close();
+
+    f.open("gen2.in");
+    f << "2 3\n";
+    f.close();
+}
