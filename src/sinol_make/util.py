b3eed25c4f536e30ad035e182eca513209e37a3f
Mateusz Masiarz <matimasiarz@gmail.com>
2024-04-18 12:07:08 +0200

    Add tests

--- src/sinol_make/util.py
+++ src/sinol_make/util.py
@@ -95,6 +95,7 @@ def save_config(config):
         "title_en",
         "sinol_task_id",
         "sinol_contest_type",
+        "sinol_static_tests",
         "sinol_undocumented_time_tool",
         "sinol_undocumented_test_limits",
         "memory_limit",
