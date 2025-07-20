2810d1529c715231ac8908bb8b3986bce690116b
Mateusz Masiarz <matimasiarz@gmail.com>
2025-07-20 17:10:56 +0200

    Add detection for pipx installation method in update messages (#299)

--- src/sinol_make/__init__.py
+++ src/sinol_make/__init__.py
@@ -109,16 +109,17 @@ def main():
                              'https://github.com/sio2project/sinol-make/#reporting-bugs-and-contributing-code')
     finally:
         if new_version is not None:
+            install_method = 'pipx' if util.is_probably_installed_by_pipx() else 'pip'
             if not util.is_dev(new_version):
                 print(util.warning(
                     f'New version of sinol-make is available (your version: {__version__}, available version: '
                     f'{new_version}).\n'
                     f'Changelog can be found at https://github.com/sio2project/sinol-make/releases.\n'
-                    f'You can update sinol-make by running `pip install sinol-make --upgrade`.'))
+                    f'You can update sinol-make by running `{install_method} install sinol-make --upgrade`.'))
             elif util.is_dev(new_version):
                 print(util.warning(
                     f'New development version of sinol-make is available (your version: {__version__}, available '
                     f'version: {new_version}).\n'
                     f'Changelog can be found at https://github.com/sio2project/sinol-make/releases.\n'
-                    f'You can update sinol-make by running `pip install sinol-make --pre --upgrade`.'
+                    f'You can update sinol-make by running `{install_method} install sinol-make --pre --upgrade`.'
                 ))
